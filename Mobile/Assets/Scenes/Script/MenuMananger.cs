7c31fd5e60db9b092448e98802e73ac477748c3f
Arthur da Cruz <cruzz16.mr@gmail.com>
2025-04-08 11:38:34 -0300

    add unity

---
+++ Mobile/Assets/Scenes/Script/MenuMananger.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MenuMananger : MonoBehaviour
+{
+public void Play()
+    {
+
+    }
+public void Options()
+    {
+
+    }
+public void Quit()
+    {
+        Application.Quit();
+    }
+}
